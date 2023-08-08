0559e0cf443d9487eb68a8e6bc36766e3a67e3f1
“alishamuneer” <it.alishamuneer@gmail.com>
2023-08-08 23:33:12 +0500

    navbar continue

--- src/layouts/clients/NabBar/TopBar.jsx
+++ src/layouts/clients/NabBar/TopBar.jsx
@@ -1,37 +1,87 @@
 import React from 'react'
-import { AppBar, Box, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import { AppBar, Box, Toolbar, Typography, Button, InputBase, Stack } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import { styled, alpha } from '@mui/material/styles';
+import logo from "../../../assets/logo-dt.svg"
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 
 
 const TopBar = () => {
   const Navbar = styled(AppBar)(({ theme }) => ({
-    height : '20vh',
+    height: '20vh',
     color: 'black',
-    background : "none"
+    background: "none"
+
+  }));
+
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 1),
+    height: '100%',
+    position: 'absolute',
+    right: 0,
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    background: 'black',
+    color: 'white'
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+      [theme.breakpoints.up('sm')]: {
+        width: '34ch',
+        '&:focus': {
+          width: '20ch',
+        },
+      },
+    },
     
   }));
+
   return (
     <>
       <Box sx={{ flexGrow: 1 }}>
         <Navbar position="static">
           <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="menu"
-              sx={{ mr: 2 }}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
+            <Stack spacing={5} direction="row">
+              <img src={logo} alt="logo" />
+              <Search>
+                <Stack>
+                  <StyledInputBase
+                    placeholder="Search David Jones"
+                    inputProps={{ 'aria-label': 'search' }}
+                  />
+                  <SearchIconWrapper>
+                    <SearchOutlinedIcon />
+                  </SearchIconWrapper>
+                </Stack>
+              </Search>
+            </Stack>
           </Toolbar>
         </Navbar>
-      </Box>  
+      </Box>
     </>
   )
 }
